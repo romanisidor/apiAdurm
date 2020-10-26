bc06dfa1b8c74cfab5d258e4e4b6f1bdae120678
Roman <18307024@utcgg.edu.mx>
2020-10-25 22:02:23 -0600

    deploy

---
+++ dist/server.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = _interopRequireWildcard(require("express"));
+
+var _index = _interopRequireDefault(require("./routes/index.routes"));
+
+var _task = _interopRequireDefault(require("./routes/task.routes"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+var cors = require('cors');
+
+var app = (0, _express["default"])(); //Routes
+
+//Settings
+//Es importante instalar el módulo de CORS para poder hacer peticiones desde otros sitios
+app.use(cors());
+app.set('port', process.env.PORT || 3000); //Middlewares
+
+app.use((0, _express.json)()); //Routes
+
+app.use(_index["default"]);
+app.use('/aduanas', _task["default"]);
+var _default = app;
+exports["default"] = _default;
